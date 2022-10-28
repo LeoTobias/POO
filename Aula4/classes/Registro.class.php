c587234ea7c92d1c62b8bfebb4d04408e26cbf89
Leonardo Tobias <tobiasleonardo7@gmail.com>
2022-10-27 23:58:46 -0300

    Finalizando Avaliação 11

--- Aula4/classes/Registro.class.php
+++ Aula4/classes/Registro.class.php
@@ -1,9 +1,9 @@
 <?php
 
-require_once("DatabaseConnection.class.php");
+require_once("dbConnection.class.php");
 require_once("src/PHPMailer.php");
 require_once("src/SMTP.php");
-require_once("src/Exception.php");
+require_once("src/Exception.php"); 
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
--- Aula4/classes/dbConnection.class.php
+++ Aula4/classes/dbConnection.class.php
@@ -40,8 +40,8 @@ class DatabaseConnection{
         $this->setDBDRIVE('mysql');
         $this->setDBHOST('localhost');
         $this->setDBNAME('protocolo');
-        $this->setDBUSER('Lupus');
-        $this->setDBPASS('SL-221B-VAS');
+        $this->setDBUSER('root');
+        $this->setDBPASS('');
     }
 
     /**
@@ -167,24 +167,3 @@ class DatabaseConnection{
     }
 
 }
-
-/*
-CREATE DATABASE Protocolo;
-CREATE TABLE usuarios(
-	id_cli int unsigned AUTO_INCREMENT PRIMARY KEY,
-  	login varchar(50),
-  	email varchar(50),
-  	telefone varchar(12),
-  	codAcesso varchar(6),
-  	validado varchar(1),
-  	tipo varchar(1)
-);
-CREATE TABLE clientes(
-	id_cliente int unsigned AUTO_INCREMENT PRIMARY KEY,
-  	nome varchar(50),
-  	email varchar(50),
-  	celular varchar(12),
-  	sexo varchar(1),
-  	rg varchar(9),
-  	cpf varchar(11)
-)
